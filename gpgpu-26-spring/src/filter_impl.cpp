c147e4c00b97d8bb49df6aade7ac9811ee860b24
Ivan HUARD <ivcvhb@gmail.com>
2026-06-27 18:46:38 +0200

    feat: update baseline

--- gpgpu-26-spring/src/filter_impl.cpp
+++ gpgpu-26-spring/src/filter_impl.cpp
@@ -15,6 +15,8 @@ struct rgb {
 #define RGB_DIFF_THRESHOLD 20
 #define MAX_WEIGHTS 100
 #define RADIUS 1
+#define LOW 30
+#define HIGH 40
 
 int res_width = 0;
 int res_height = 0;
@@ -68,14 +70,14 @@ int max_res(const std::vector<std::array<reservoir, K>>& rs, int idx) {
     return max_idx;
 }
 
-uint8_t get_gray_pixel(uint8_t* buffer, int x, int y, int stride)
+uint8_t get_gray_pixel(uint8_t* buffer, int x, int y, int stride, int pixel_stride)
 {
-    rgb* lineptr = (rgb*) (buffer + y * stride);
-    return lineptr[x].r;
+    uint8_t* pixel = buffer + y * stride + x * pixel_stride;
+    return pixel[0];
 }
 
 void erosion(uint8_t* buffer, std::vector<uint8_t>& eroded,
-                int width, int height, int stride, int radius)
+                int width, int height, int stride, int pixel_stride, int radius)
 {
     eroded.resize(width * height);
 
@@ -95,7 +97,7 @@ void erosion(uint8_t* buffer, std::vector<uint8_t>& eroded,
                         continue;
                     }
 
-                    uint8_t value = get_gray_pixel(buffer, xx, yy, stride);
+                    uint8_t value = get_gray_pixel(buffer, xx, yy, stride, pixel_stride);
                     min_value = std::min(min_value, value);
                 }
             }
@@ -106,10 +108,10 @@ void erosion(uint8_t* buffer, std::vector<uint8_t>& eroded,
 }
 
 void dilatation(const std::vector<uint8_t>& input, uint8_t* buffer,
-                 int width, int height, int stride, int radius)
+                 int width, int height, int stride, int pixel_stride, int radius)
 {
     for (int y = 0; y < height; ++y) {
-        rgb* lineptr = (rgb*) (buffer + y * stride);
+        uint8_t* lineptr = buffer + y * stride;
 
         for (int x = 0; x < width; ++x) {
             uint8_t max_value = 0;
@@ -131,7 +133,83 @@ void dilatation(const std::vector<uint8_t>& input, uint8_t* buffer,
                 }
             }
 
-            lineptr[x] = rgb{max_value, max_value, max_value};
+            lineptr[x * pixel_stride] = max_value;
+            lineptr[x * pixel_stride + 1] = max_value;
+            lineptr[x * pixel_stride + 2] = max_value;
+        }
+    }
+}
+
+void hysteresis(uint8_t* buffer, int width, int height, int stride, int pixel_stride, int low, int high)
+{
+    static std::vector<bool> marker;
+    marker.assign(width * height, false);
+
+    static std::vector<bool> candidate;
+    candidate.assign(width * height, false);
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+
+            int i = y * width + x;
+            uint8_t value = buffer[y * stride + x * pixel_stride];
+
+            candidate[i] = value >= low;
+            marker[i] = value >= high;
+
+            buffer[y * stride + x * pixel_stride] = 0;
+            buffer[y * stride + x * pixel_stride + 1] = 0;
+            buffer[y * stride + x * pixel_stride + 2] = 0;
+        }
+    }
+
+    bool changed = true;
+
+    while (changed)
+    {
+        changed = false;
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+
+                int i = y * width + x;
+
+                if (buffer[y * stride + x * pixel_stride]) continue;
+
+                if (!candidate[i]) continue;
+
+                if (marker[i])
+                {
+                    buffer[y * stride + x * pixel_stride] = 255;
+                    buffer[y * stride + x * pixel_stride + 1] = 255;
+                    buffer[y * stride + x * pixel_stride + 2] = 255;
+
+                    changed = true;
+                    continue;
+                }
+
+                for (int dy = -1; dy <= 1; ++dy) {
+                    for (int dx = -1; dx <= 1; ++dx)
+                    {
+                        if (dx == 0 && dy == 0) continue;
+
+                        int yy = y + dy;
+                        int xx = x + dx;
+
+                        if (yy < 0 || yy >= height || xx < 0 || xx >= width) continue;
+
+                        if (buffer[yy * stride + xx * pixel_stride])
+                        {
+                            buffer[y * stride + x * pixel_stride] = 255;
+                            buffer[y * stride + x * pixel_stride + 1] = 255;
+                            buffer[y * stride + x * pixel_stride + 2] = 255;
+
+                            changed = true;
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -140,6 +218,8 @@ extern "C" {
 
     void filter_impl(uint8_t* buffer, int width, int height, int stride, int pixel_stride)
     {
+
+
         // STEP 1 : difference
         if (res_height != height || res_width != width) {
             res_height = height;
@@ -158,10 +238,15 @@ extern "C" {
         }
         for (int y = 0; y < height; ++y)
         {
-            rgb* lineptr = (rgb*) (buffer + y * stride);
             for (int x = 0; x < width; ++x)
             {
-                rgb p = lineptr[x];
+                uint8_t* lineptr = buffer + y * stride + x * pixel_stride;
+                rgb p = rgb{
+                    lineptr[0],
+                    lineptr[1],
+                    lineptr[2]
+                };
+
                 int idx = y * width + x;
                 int m_idx = find_matching_reservoir(p, rs, idx);
 
@@ -176,13 +261,18 @@ extern "C" {
                         unsigned int tmpR1 = (unsigned int)rs[idx][m_idx].rgbV.r * (rs[idx][m_idx].w - 1) + (unsigned int)p.r;
                         unsigned int tmpG1 = (unsigned int)rs[idx][m_idx].rgbV.g * (rs[idx][m_idx].w - 1) + (unsigned int)p.g;
                         unsigned int tmpB1 = (unsigned int)rs[idx][m_idx].rgbV.b * (rs[idx][m_idx].w - 1) + (unsigned int)p.b;
-                        uint8_t tmpR = (uint8_t)(tmpR1 / rs[idx][m_idx].w);
-                        uint8_t tmpG = (uint8_t)(tmpG1 / rs[idx][m_idx].w);
-                        uint8_t tmpB = (uint8_t)(tmpB1 / rs[idx][m_idx].w);
-                        rs[idx][m_idx].rgbV = rgb{tmpR, tmpG, tmpB};
+                        rs[idx][m_idx].rgbV.r = (uint8_t)(tmpR1 / rs[idx][m_idx].w);
+                        rs[idx][m_idx].rgbV.g = (uint8_t)(tmpG1 / rs[idx][m_idx].w);
+                        rs[idx][m_idx].rgbV.b = (uint8_t)(tmpB1 / rs[idx][m_idx].w);
                     }
                     else {
-                        rs[idx][m_idx].rgbV = p;
+                        // Keep updating with moving average even when max weight reached
+                        unsigned int tmpR1 = (unsigned int)rs[idx][m_idx].rgbV.r * (MAX_WEIGHTS - 1) + (unsigned int)p.r;
+                        unsigned int tmpG1 = (unsigned int)rs[idx][m_idx].rgbV.g * (MAX_WEIGHTS - 1) + (unsigned int)p.g;
+                        unsigned int tmpB1 = (unsigned int)rs[idx][m_idx].rgbV.b * (MAX_WEIGHTS - 1) + (unsigned int)p.b;
+                        rs[idx][m_idx].rgbV.r = (uint8_t)(tmpR1 / MAX_WEIGHTS);
+                        rs[idx][m_idx].rgbV.g = (uint8_t)(tmpG1 / MAX_WEIGHTS);
+                        rs[idx][m_idx].rgbV.b = (uint8_t)(tmpB1 / MAX_WEIGHTS);
                     }
                 }
                 else if (m_idx != -1 && rs[idx][m_idx].w == 0) { // empty
@@ -200,20 +290,47 @@ extern "C" {
                         rs[idx][min_idx].w = 1;
                     }
                 }
-                rgb background = rs[idx][max_res(rs, idx)].rgbV;
-                int score = std::max(std::max(abs((int)p.r - (int)background.r),
-                                      abs((int)p.g - (int)background.g)),
-                                      abs((int)p.b - (int)background.b));
-                lineptr[x] = rgb{(uint8_t)std::min(score, 255),
-                                 (uint8_t)std::min(score, 255),
-                                 (uint8_t)std::min(score, 255)};
+/*
+
+                */
+                bool established = false;
+                #define BG_MIN_WEIGHT 30
+                int score = 255;
+                for (int i = 0; i < K; ++i) {
+                    if (rs[idx][i].w >= BG_MIN_WEIGHT) {       // mode établi, pas un réservoir tout neuf
+                        established = true;
+                        int d = std::max(std::max(
+                                    abs((int)p.r - (int)rs[idx][i].rgbV.r),
+                                    abs((int)p.g - (int)rs[idx][i].rgbV.g)),
+                                    abs((int)p.b - (int)rs[idx][i].rgbV.b));
+                        score = std::min(score, d);            // distance au mode le plus proche
+                    }
+                }
+                uint8_t value = static_cast<uint8_t>(std::min(score, 255));
+                if (!established) {
+                    /*
+                    rgb background = rs[idx][max_res(rs, idx)].rgbV;
+                    score = std::max(std::max(abs((int)p.r - (int)background.r),
+                                          abs((int)p.g - (int)background.g)),
+                                          abs((int)p.b - (int)background.b));
+                    */
+                    uint8_t value = static_cast<uint8_t>(0);
+                }
+
+                lineptr[0] = value;
+                lineptr[1] = value;
+                lineptr[2] = value;
             }
         }
+
         // STEP 2 : Ouverture
         static std::vector<uint8_t> eroded;
 
-        erosion(buffer, eroded, width, height, stride, RADIUS);
-        dilatation(eroded, buffer, width, height, stride, RADIUS);
+        erosion(buffer, eroded, width, height, stride, pixel_stride, RADIUS);
+        dilatation(eroded, buffer, width, height, stride, pixel_stride, RADIUS);
+
+        // STEP 3: Seuillage d’hystérésis
+        hysteresis(buffer, width, height, stride, pixel_stride, LOW, HIGH);
 
         // You can fake a long-time process with sleep
         {
